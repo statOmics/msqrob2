51738d9807da32447038ec0253c2413addfbc448
Alexey Stukalov <astukalol@seer.bio>
2025-05-26 14:51:30 -0700

    msqrobLm(): rename data arg to colsMetadata
    as that's what it is

--- R/msqrob.R
+++ R/msqrob.R
@@ -57,9 +57,10 @@ msqrobLm <- function(y,
     robust = TRUE,
     maxitRob = 5) {
     myDesign <- model.matrix(formula, data)
+    # apply the model to each protein
     models <- apply(y, 1,
         function(y, design) {
-            ## computatability check
+            ## computability check
             obs <- is.finite(y)
             type <- "fitError"
             model <- list(
